30eccf98044eb788e85d85e595ed0c76cd3bcabc
Fathony L <fathony@smart-leaders.net>
2024-09-28 11:20:17 +0700

    tested: reset random.ts history into one from very old git-branch

--- src/random.ts
+++ src/random.ts
@@ -1,9 +1,12 @@
 /**
- * Original Code: https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Random.cs
+ * Original Code: 
+ *      https://github.com/microsoft/referencesource/blob/master/mscorlib/system/random.cs
+ *      https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Random.cs
  * Converted to TypeScript By: x2nie
  * Date: 2023-11-19
  * 
  */
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
@@ -11,38 +14,36 @@ const Int32 = {
     MinValue: Math.pow(2, 31) * -1,
     MaxValue: Math.pow(2, 31) - 1
 }
-const MBIG =  Int32.MaxValue;
+const MBIG = Int32.MaxValue;
 const MSEED = 161803398;
-
-const CLAMPER = new Float32Array(1); // global temporary var to mimic C#'s Double.
-const OneBIGth =  1.0/MBIG;
 
 export class Random {
     //
     // Member Variables
     //
-    private inext:number;
-    private inextp:number;
-    private SeedArray = new Array(56);
+    private inext: number;
+    private inextp: number;
     //private int[] SeedArray = new int[56];
+    private SeedArray: Int32Array;
 
     // public Random(Seed:number) {}
-    constructor(Seed:number|undefined=undefined) {
+    constructor(seed: number | undefined = undefined) {
         this.inext = 0;
         this.inextp = 21;
+        this.SeedArray = new Int32Array(56);
         this.SeedArray.fill(0);
 
-        if(Seed===undefined){
+        if (seed === undefined) {
             //this(Environment.TickCount);
-            Seed = Math.floor(Math.random() * MBIG);
+            seed = Math.floor(Math.random() * MBIG);
         }
-        Seed = Math.floor(Seed); // = int()Seed
-        
-        let ii:number;
-        let mj:number, mk:number;
+        seed = Math.floor(seed); // = int()Seed
+
+        let ii: number;
+        let mj: number, mk: number;
         //Initialize our Seed array.
         //This algorithm comes from Numerical Recipes in C (2nd Ed.)
-        let subtraction = (Seed == Int32.MinValue) ? Int32.MaxValue : Math.abs(Seed);
+        let subtraction = (seed == Int32.MinValue) ? Int32.MaxValue : Math.abs(seed);
         mj = MSEED - subtraction;
         this.SeedArray[55] = mj;
         mk = 1;
@@ -59,68 +60,65 @@ export class Random {
                 if (this.SeedArray[i] < 0) this.SeedArray[i] += MBIG;
             }
         }
-        this.inext = 0;
-        this.inextp = 21;
-        // Seed = 1;
     }
 
-    public next(maxValue:number=0):number {
-        if(maxValue==0)
+    public next(maxValue: number = 0): number {
+        if (maxValue == 0)
             return this.internalSample()
 
-        if (maxValue<0) {
+        if (maxValue < 0) {
             throw new Error("ArgumentOutOfRange_MustBePositive");
         }
-        // Contract.EndContractBlock();
-        return Math.floor(this.sample()*maxValue);
+        return Math.floor(this.sample() * maxValue);
     }
 
-    public nextDouble():number {
-        // return 0.9999
+    public nextDouble(): number {
         return this.sample();
     }
 
-    protected sample():number { //Float.double
+    protected sample(): number { //Float.double
         //Including this division at the end gives us significantly improved
         //random number distribution.
 
         //? C# :
         // return (this.InternalSample()*(1.0/MBIG));
 
+        //? however, it actually produces identical result with C# (tested on Chrome | Ubuntu 24.04 x64 | .NET v8.0.108 | 2024-09-28)
+        return this.internalSample() * (1.0 / MBIG);
+
         //? Old Implementation & Slow :
-        // const jsFloat = this.InternalSample()*(1.0/MBIG);
+        // const jsFloat = this.internalSample() * (1.0 / MBIG);
         // const csharpDouble = jsFloat.toPrecision(15); //got string
-        // const n = Number(csharpDouble); // will identical to C# value
-        // return n;
+        // return Number(csharpDouble); // will identical to C# value
 
         //? Faster JavaScript & identical to C# :
         // const f32arr = new Float32Array(1)
-        // f32arr[0] = this.InternalSample()*(1.0/MBIG);
+        // f32arr[0] = this.internalSample()*(1.0/MBIG);
         // return f32arr[0]
 
-        //? same as above, but with reuse, like its fastest
-        CLAMPER[0] = this.internalSample()*OneBIGth;
-        return CLAMPER[0]
+        //? same as above, but with reuse, like its fastest, but is not thread-safe
+        // CLAMPER[0] = this.internalSample()*OneBIGth;
+        // return CLAMPER[0]
     }
 
-    private internalSample():number { // int
-        let retVal:number;
+    private internalSample(): number { // int
+        let retVal: number;
         let locINext = this.inext;
         let locINextp = this.inextp;
 
-        if (++locINext >=56) locINext=1;
-        if (++locINextp>= 56) locINextp = 1;
-        
-        retVal = this.SeedArray[locINext]-this.SeedArray[locINextp];
+        if (++locINext >= 56) locINext = 1;
+        if (++locINextp >= 56) locINextp = 1;
 
-        if (retVal == MBIG) retVal--;          
-        if (retVal<0) retVal+=MBIG;
-        
-        this.SeedArray[locINext]=retVal;
+        retVal = this.SeedArray[locINext] - this.SeedArray[locINextp];
+
+        if (retVal == MBIG) retVal--;
+        if (retVal < 0) retVal += MBIG;
+
+        this.SeedArray[locINext] = retVal;
 
         this.inext = locINext;
         this.inextp = locINextp;
-                  
+
         return retVal;
     }
 }
