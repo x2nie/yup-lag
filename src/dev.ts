4d3aec0a6a16544d3ba85c5a875e45444c938177
Fathony L <fathony@smart-leaders.net>
2024-10-06 18:16:31 +0700

    experimental: works ! as expected

--- src/dev.ts
+++ src/dev.ts
@@ -1,1 +1,40 @@
-console.log('vite oke!')
+import { DOMParser } from "@xmldom/xmldom";
+import { Interpreter } from "./interpreter"
+
+document.getElementById('btn1').addEventListener('click', exec)
+const code1 = document.getElementById('code1') as HTMLTextAreaElement
+const pre1 = document.getElementById('result1') as HTMLPreElement
+
+const MX = 11, MY = 11, MZ =1;
+
+async function exec(){
+    console.log('vite oke!',code1)
+    const elem = xmlParse(code1.value)
+    const ip = await Interpreter.load(
+        elem, MX, MY, MZ
+    )
+    const curr = ip.run(undefined, 5)
+    let result = curr.next();
+    while(!result.done){
+        console.log(result)
+        result = curr.next();
+    }
+    const [grid] = ip.state()
+    let s = ''
+    let i = 0
+    for (let y = 0; y < MY; y++) {
+        for (let x = 0; x < MX; x++) {
+            s += grid[i] == 0 ? '▒' : '█';
+            i++;
+        }
+        s += '\n'
+    }
+    pre1.textContent = s;
+    // debugger
+}
+
+function xmlParse(text: string) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/xml");
+    return doc.documentElement;
+}
